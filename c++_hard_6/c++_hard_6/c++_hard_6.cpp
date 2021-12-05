1e1abb2b95fca47dd83495a336721d471479e95b
razor-roman <roman.it.kirov@yandex.ru>
2021-12-05 15:51:47 +0300

    1.1

--- c++_hard_6/c++_hard_6/c++_hard_6.cpp
+++ c++_hard_6/c++_hard_6/c++_hard_6.cpp
@@ -1,20 +1,112 @@
-﻿// c++_hard_6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include<iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <future>
+#include<algorithm>
+#include <sstream>
+#include <execution>
+#include<vector>
+#include <future>
+#include<queue>
+using namespace std;
 
-#include <iostream>
-
-int main()
+//1
+class pcout : public std::ostringstream
 {
-    std::cout << "Hello World!\n";
+public:
+    ~pcout()
+    {
+        lock_guard<mutex> guard(mutex);
+        cout << this->str() << endl;
+    }
+private:
+    static std::mutex _mutexPrint;
+};
+std::mutex pcout::_mutexPrint{};
+//2
+int primeNumber(int n)
+{
+    vector<int> primeNumber;
+    int y = 2;
+    while(primeNumber.size()<n) {
+                 
+                for (size_t i = 2; i <= y; i++)
+                {                    
+                    if (y % i == 0 && i < y) break;
+                    if (i == y || y % i == 0)
+                    {
+                        primeNumber.push_back(i);
+                        cout << primeNumber.back() << "" << endl;
+                    }
+                }
+        y++;
+    }
+    thread::id thread_id = this_thread::get_id();
+    cout << "primeNumber thread id: " << thread_id << endl;
+    return primeNumber.back();
+}
+//3
+
+queue<string> items;
+mutex m;
+condition_variable semophore;
+void owner(){
+    for (size_t i = 0u; i < 5; i++)
+    {        
+        lock_guard<mutex> lock(m);
+        items.push("Vase");
+        semophore.notify_one();
+        cout << "added" << endl;
+        //---------------------
+        thread::id thread_id = this_thread::get_id();
+        cout << "primeNumber thread id: " << thread_id << endl;
+    }
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void thief() {
+    for (size_t i = 0u; i < 5; i++)
+    {
+        unique_lock<mutex> lock(m);
+        semophore.wait(lock, [] {return !items.empty(); });
+        cout << items.front() << endl;
+        items.pop();
+        //---------------------
+        thread::id thread_id = this_thread::get_id();
+        cout << "primeNumber thread id: " << thread_id << endl;
+    }
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+int main()
+{   
+    //1
+    {
+        pcout prt;
+        string val = "val";
+        prt << "str";
+        pcout{} << "my_val=" << val << std::endl;
+        pcout{} << "my_val=" << "123" << std::endl;
+        
+    }
+
+    //2
+    {
+        
+        auto future = std::async(primeNumber, 100);
+        int primeNumber = future.get();
+        cout << "your prime number is=" << primeNumber << endl;
+        thread::id thread_id = this_thread::get_id();
+        cout << "Main thread id: " << thread_id << endl;
+
+    }
+    //3
+    {
+       thread th1(owner);
+       thread th2(thief);
+       th1.join();
+       th2.join();
+    }
+    
+    
+}
